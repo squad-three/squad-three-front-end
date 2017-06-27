22fc396f6325670efe78a87a9d225ecba40573a8
EricLScace <mail@ericscace.com>
2017-06-27 16:18:19 -0400

    Defer loading bucket list until after user logs-in
    What:
    * created a bucketlist init function
    
    Consequences:
    * Need to clear the bucket when user logs out
    * Clear during password change? (defer?)
    
    Tests:
    OK for login
    OK for registered new users.
    
    Reference #57 #60

---
+++ assets/scripts/bl-init.js
@@ -0,0 +1,52 @@
+'use strict'
+// Initializes the table space after user logs in
+const bucketList = require('../templates/bucketList.handlebars')
+
+require('datatables.net-bs')
+require('datatables.net-buttons')
+require('datatables.net-select')
+require('./dataTables.editor.js')
+
+const blInit = function () {
+  // Instantiate the bucket list editor
+  const editor = new $.fn.dataTable.Editor({
+    table: '#main-table',
+    fields: [
+      {label: 'Description:', name: 'Description'},
+      {label: 'Category:', name: 'category'},
+      {label: 'Location:', name: 'location'},
+      {label: 'Duration:', name: 'duration'},
+      {label: 'Cost:', name: 'date', type: 'datetime'},
+      {type: 'select',
+        label: 'Status:',
+        name: 'status',
+        options: [{ label: 'Some Day', value: 'Some Day' },
+                  { label: 'In Progress', value: 'In Progress' },
+                  { label: 'Done', value: 'Done' }
+        ]
+      }
+    ]
+  })
+
+  // load DOM with the initial bucketList
+  $('#bucket').html(bucketList())
+  $('#main-table').DataTable({
+    select: true,
+    dom: 'Bfrtip',
+    buttons: [
+      {extend: 'create', editor: editor},
+      {extend: 'edit', editor: editor},
+      {extend: 'remove', editor: editor}
+    ],
+    columns: [
+      {data: 'description'},
+      {data: 'category'},
+      {data: 'location'},
+      {data: 'duration'},
+      {data: 'cost'},
+      {data: 'status'}
+    ]
+  })
+}
+
+module.exports = {blInit}
