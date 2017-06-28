3949a8b3b9fdf1ead407e4045cb8a1c22046ef4f
EricLScace <mail@ericscace.com>
2017-06-28 12:34:11 -0400

    Generate DT message to /bucket
    References #70

--- assets/scripts/bl-init.js
+++ assets/scripts/bl-init.js
@@ -1,5 +1,6 @@
 'use strict'
 // Initializes the table space after user logs in
+const blInitParams = require('./bl-init-params')
 const bucketList = require('../templates/bucketList.handlebars')
 
 require('datatables.net-bs')
@@ -8,65 +9,13 @@ require('datatables.net-select')
 require('./dataTables.editor.js')
 
 const blInit = function () {
-  // Instantiate the bucket list editor
-  const editor = new $.fn.dataTable.Editor({
-    table: '#main-table',
-    fields: [
-      {label: 'Description:', name: 'description'},
-      {type: 'select',
-        label: 'Category:',
-        name: 'category',
-        options: [{ label: 'Acheivement', value: 'Acheivement' },
-                  { label: 'Adventure', value: 'Adventure' },
-                  { label: 'Body & Health', value: 'Body & Health' },
-                  { label: 'Career', value: 'Career' },
-                  { label: 'Charity', value: 'Charity' },
-                  { label: 'Creative', value: 'Creative' },
-                  { label: 'Cultural', value: 'Cultural' },
-                  { label: 'Events', value: 'Events' },
-                  { label: 'Experience', value: 'Experience' },
-                  { label: 'Family & Kids', value: 'Family & Kids' },
-                  { label: 'Financial', value: 'Financial' },
-                  { label: 'Food & Drinks', value: 'Food & Drinks' },
-                  { label: 'Personal Development', value: 'Personal Development' },
-                  { label: 'Relationships', value: 'Relationships' },
-                  { label: 'Sports', value: 'Sports' },
-                  { label: 'Travel', value: 'Travel' }
-        ]
-      },
-      {label: 'Location:', name: 'location'},
-      {label: 'Duration:', name: 'duration'},
-      {label: 'Cost:', name: 'cost'},
-      {type: 'select',
-        label: 'Status:',
-        name: 'status',
-        options: [{ label: 'Some Day', value: 'Some Day' },
-                  { label: 'In Progress', value: 'In Progress' },
-                  { label: 'Done', value: 'Done' }
-        ]
-      }
-    ]
-  })
+  // Instantiate the bucket list editor with editor config object
+  const editor = new $.fn.dataTable.Editor(blInitParams.editorConfigurationParameters())
 
-  // load DOM with the initial bucketList
+  // load DOM with the initial bucketList with main table parameters
+  //  object, passing in the reference to the instantiated editor.
   $('#bucket').html(bucketList())
-  $('#main-table').DataTable({
-    select: true,
-    dom: 'Bfrtip',
-    buttons: [
-      {extend: 'create', editor: editor},
-      {extend: 'edit', editor: editor},
-      {extend: 'remove', editor: editor}
-    ],
-    columns: [
-      {data: 'description'},
-      {data: 'category'},
-      {data: 'location'},
-      {data: 'duration'},
-      {data: 'cost'},
-      {data: 'status'}
-    ]
-  })
+  $('#main-table').DataTable(blInitParams.mainTableParameters(editor))
 }
 
 module.exports = {blInit}
