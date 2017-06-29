30ed488b68a47f7856282dbf9e4440ac7c633270
EricLScace <mail@ericscace.com>
2017-06-29 03:52:55 -0400

    Use editor.close on logout
    Why: fixes bug that prevented edit/new/delete buttons from
    working on re-loggin in.
    
    What: placed editor in store (blInit) so that it could be
    used at logout.
    
    Closes #95

--- assets/scripts/bl-init.js
+++ assets/scripts/bl-init.js
@@ -2,6 +2,7 @@
 // Initializes the table space after user logs in
 const blInitParams = require('./bl-init-params')
 const bucketList = require('../templates/bucketList.handlebars')
+const store = require('./store')
 
 require('datatables.net-bs')
 require('datatables.net-buttons')
@@ -11,6 +12,7 @@ require('./dataTables.editor.js')
 const blInit = function () {
   // Instantiate the bucket list editor with editor config object
   const editor = new $.fn.dataTable.Editor(blInitParams.editorConfigurationParameters())
+  store.editor = editor
 
   // load DOM with the initial bucketList with main table parameters
   //  object, passing in the reference to the instantiated editor.
